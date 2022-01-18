6c7e72cab653f86673c355cf8fe5d0233db989d8
Abdullo92 <abdullo.giyasov@gmail.com>
2022-01-18 22:56:29 +0500

    use value the range

---
+++ script.js
@@ -0,0 +1,8 @@
+function useRangeValue() {
+      let range_1 = document.querySelector('#range_1')
+      let showVal = document.querySelector('#show_value')
+      showVal.innerHTML = range_1.value;
+
+}
+
+
